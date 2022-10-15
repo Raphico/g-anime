36354aaa4dd8b97a662a317910c70f7734d999d7
Raphico <RaphicoGit@gmail.com>
2022-10-15 10:48:09 -0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const connectDb = require('./config/db');
 const errorHandler = require('./middleware/errorMiddleware');
 const protect = require('./middleware/authMiddleware');
-const path = require('path');
+const cors = require('cors');
 
 connectDb();
 
@@ -11,23 +11,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api/watchList', require('./routes/api/watchListRouter'));
 app.use('/api/user', require('./routes/api/usersRouter'));
 
-if (process.env.NODE_ENV === 'production')
-{
-  app.use(express.static(path.join(__dirname, '../', 'frontend', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
-  })
-}
-else
-{
-  app.get('/', (req, res) => {
-    res.send('Not in production');
-  })
-}
+app.get('/', (req, res) => {
+  res.send('Server Started');
+})
 
 app.use(errorHandler);
 app.use(protect);
